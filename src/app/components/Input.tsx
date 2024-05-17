524031ee4029edfebfc21276547479362a7f11c4
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-17 13:12:58 -0300

    Adicionando placeholder no formulário

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,12 +1,13 @@
 interface InputProps {
   text: string
+  placeholder: string
 }
 
 export const Input = (props: InputProps) => {
   return (
     <div className='flex flex-col w-full'>
-      <label className='font-medium'>{props.text}</label>
-      <input className='rounded-md bg-slate-300 p-2' />
+      <label className='font-medium text-zinc-500'>{props.text}</label>
+      <input className='rounded-md bg-slate-300 p-2 focus:outline-none' placeholder={props.placeholder} />
     </div>
   )
 }
--- src/app/participar/page.tsx
+++ src/app/participar/page.tsx
@@ -4,17 +4,17 @@ import { Button, Type } from "../components/Button";
 export default function Page() {
   return (
     <section>
-      <h1 className='text-amber-600'>Seja um médico do barulho!</h1>
+      <h1 className='text-amber-600'>Seja um Médico do Barulho!</h1>
       <h2 className='text-amber-600'>
         Junte-se a nós para espalhar sorrisos! Torne-se um Médico do Barulho e faça a diferença na vida de pacientes hospitalares.
         Juntos, podemos levar alegria onde ela é mais necessária.
       </h2>
       <form className='w-1/2'>
         <div className='flex gap-4'>
-          <Input text='Nome' />
-          <Input text='Telefone' />
+          <Input text='Seu nome' placeholder="Piolhin" />
+          <Input text='Seu telefone' placeholder="(32) 90000-0000" />
         </div>
-        <Input text='Email' />
+        <Input text='Seu email' placeholder="piolhin@gmail.com" />
         <div className='w-full mt-4'>
           <Button text="Enviar" link="/" type={Type.Main} />
         </div>
