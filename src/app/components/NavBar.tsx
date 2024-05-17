c92cb8b8d93e72bbdadcc8ab966470dc2ceee64b
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-17 15:34:32 -0300

    Finalização do estilo navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -22,7 +22,7 @@ export const NavBar = () => {
           <img src="https://scontent.fjdf2-2.fna.fbcdn.net/v/t1.18169-9/12745648_675211279284222_7597893156519552135_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFiFb5g0HqwtrAj0Ye9BYqtPQRo1e1kbSo9BGjV7WRtKiw7ioeXcexMK6E2CXuFdXN2cYhw1ZimeLOkFRLY3kbV&_nc_ohc=E8IulPNrHHQQ7kNvgHWSQjp&_nc_ht=scontent.fjdf2-2.fna&oh=00_AYCFUajIYpHFHG5N0U6OY28S2VJpA7LCc8ZQHiDElGoZgw&oe=666C18EB" className="h-8" alt="Logo médicos do barulho" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap uppercase text-sky-400">Médicos do Barulho</span>
         </a>
-        <button id='abrir-menu' data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400" aria-controls="navbar-default" aria-expanded="false">
+        <button id='abrir-menu' data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-amber-600 dark:text-amber-600" aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Abrir menu principal</span>
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
