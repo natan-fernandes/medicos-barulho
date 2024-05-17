835c25ca9b043f9d751abf5aa023cf11352d0736
phellipeduarte <phellipe.duarte@icloud.com>
2024-05-17 15:28:19 -0300

    Adicionando link ao footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,10 @@ export const Footer = () => {
     <footer className='fixed bottom-0 w-full gap-8 bg-slate-50 border-t-4 border-amber-600 py-10 px-5'>
       <div className="flex items-center justify-center md:justify-between max-w-screen-xl mx-auto">
         <section className='prose hidden md:block'>
-          <h2 className='mb-0 text-amber-600'>Gostou da ideia? Venha fazer parte</h2>
+          <h2 className='mb-0 text-amber-600'>
+            Gostou da ideia? Venha ‎
+            <a href="/participar" className="text-sky-400 font-bold">participar</a>
+          </h2>
           <h3 className='text-amber-600'>Médicos do Barulho © 2024</h3>
         </section>
         <div className='flex gap-4 w-15'>
